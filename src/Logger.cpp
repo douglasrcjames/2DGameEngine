7ac4a475a1082206b854a1d11906bc0c464c4573
Douglas James <douglasrcjames@gmail.com>
2022-10-28 19:58:37 -0700

    Added logger!
    Based on Björn Sundin's answer on this SO: https://stackoverflow.com/questions/17223096/outputting-date-and-time-in-c-using-stdchrono

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,9 +1,22 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>
+
 #include "Logger.h"
 
-void Logger::Log(const std::string& message) {
+std::string get_current_time_and_date()
+{
+	auto const time = std::chrono::current_zone()
+		->to_local(std::chrono::system_clock::now());
+	return std::format("{:%Y-%m-%d %X}", time);
+}
 
+void Logger::Log(const std::string& message) {
+	std::string output = "LOG: [" + get_current_time_and_date() + "]: " + message;
+	std::cout << "\x1B[32m" << output << "\033[0m" << std::endl;
 }
 
 void Logger::Err(const std::string& message) {
-
+	std::string output = "ERR: [" + get_current_time_and_date() + "]: " + message;
+	std::cerr << "\x1B[91m" << output << "\033[0m" << std::endl;
 }
